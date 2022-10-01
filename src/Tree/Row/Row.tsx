b454f406179d9f895749d62aec89123a96dac084
Максим <maksim_bulavin@mail.ru>
2022-10-01 19:46:02 +0700

    init

---
+++ src/Tree/Row/Row.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import { NodeType } from "../Tree";
+import plus from "../../assets/plus.png";
+import rubbish from "../../assets/rubbish.png";
+import s from "./Row.module.css";
+
+type PropsType = {
+	node: NodeType;
+	setTree: any;
+	addRowHandler: (id: number) => void;
+	delRowHandler: (id: number) => void;
+};
+
+export const Row: React.FC<PropsType> = ({ node, addRowHandler, delRowHandler, setTree }) => {
+	const [inputValue, setInputValue] = useState(node.title);
+
+	const autoResize = (e: any) => {
+		const size = e.target.scrollWidth;
+		e.target.style.width = size + "px";
+	}
+
+	return (
+		<div className={s.wrapper}>
+			<p className={s.title}>
+				<input
+					className={s.input}
+					type="text"
+					value={inputValue}
+					onChange={(e) => setInputValue(e.target.value)}
+					onInput={autoResize}
+				/>
+			</p>
+			<button className={s.bAdd} onClick={() => setTree(addRowHandler(node.id))}>
+				<img className={s.img} src={plus} alt="plus" />
+			</button>
+			<button onClick={() => setTree(delRowHandler(node.id))}>
+				<img className={s.img} src={rubbish} alt="rubbish" />
+			</button>
+		</div>
+	);
+};
---
+++ src/Tree/Tree.tsx
@@ -0,0 +1,101 @@
+import { useState, useEffect } from "react";
+import { Row } from "./Row/Row";
+import s from "./Tree.module.css";
+
+export type NodeType = {
+	title: string;
+	id: number;
+	contains?: Array<NodeType>;
+};
+
+export const Tree = () => {
+	const [tree, setTree] = useState([] as Array<NodeType>);
+	const [uniqueId, setUniqueId] = useState(1);
+
+	const initialTree = [
+		{
+			title: "Node 1",
+			id: 0,
+		},
+	];
+
+	useEffect(() => {
+		setTree(initialTree);
+	}, []);
+
+	const addRowHandler = (id: number) => {
+		// the wrapper function is needed for the first iteration to work on the original tree
+		const wrapper = (tree: Array<NodeType>, id: number) => {
+			return tree.map((node) => {
+				if (node.id === id) {
+					//if array already exists then append node to the end
+					if (node.contains) {
+						node.contains.push({ title: "Node " + (uniqueId + 1), id: uniqueId + 1 });
+						setUniqueId(uniqueId + 1);
+					}
+					//if array doesn't exist then create
+					else {
+						node.contains = [{ title: "Node " + (uniqueId + 1), id: uniqueId + 1 }];
+						setUniqueId(uniqueId + 1);
+					}
+					return node;
+				} 
+				else if (node.contains) wrapper(node.contains, id);
+
+				return node;
+			});
+		};
+		return wrapper(tree, id);
+	};
+
+	const delRowHandler = (id: number) => {
+		// the wrapper function is needed for the first iteration to work on the original tree
+		const wrapper = (tree: Array<NodeType>, id: number) => {
+			const filteredTree = tree.filter((node) => node.id !== id);
+
+			return filteredTree.map((node) => {
+				const { contains, ...nodeWithoutContains } = node;
+				const returnObj = { ...nodeWithoutContains, contains: [] as Array<NodeType> };
+
+				if (contains) {
+					returnObj.contains = wrapper(contains, id);
+				}
+
+				return returnObj;
+			});
+		};
+
+		return wrapper(tree, id);
+	};
+
+	const buildTree = (tree: Array<NodeType>): JSX.Element[] => {
+		return tree.map((node) => {
+			if (node.contains) {
+				buildTree(node.contains);
+			}
+			return (
+				<div className={s.wrapper} key={node.id}>
+					<Row
+						node={node}
+						setTree={setTree}
+						addRowHandler={addRowHandler}
+						delRowHandler={delRowHandler}
+					/>
+					{node.contains && buildTree(node.contains)}
+				</div>
+			);
+		});
+	};
+
+	const resetHandler = () => {
+		setTree(initialTree);
+		setUniqueId(1);
+	};
+
+	return (
+		<div className={s.tree}>
+			<button className={s.bReset} onClick={resetHandler}>Reset</button>
+			{tree?.length ? buildTree(tree) : <p className={s.no_data}>No data</p>}
+		</div>
+	);
+};
